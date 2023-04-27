472dc61bf6eccca19de5973faabdbe8e140f5df2
JoaoPedroll <joaopedroll002@gmail.com>
2023-04-27 14:33:36 -0300

    Versão 1.0 atv classificação

---
+++ back_end.py
@@ -0,0 +1,35 @@
+from sklearn import svm
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn import tree
+import pickle
+
+
+dados = pd.read_csv('Cancer_Data.csv',   decimal =',', thousands = '.')
+
+dados.drop(columns=['id','Unnamed: 32','compactness_mean', 'concavity_mean',
+       'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
+       'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se',
+       'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se',
+       'fractal_dimension_se', 'radius_worst', 'texture_worst',
+       'perimeter_worst', 'area_worst', 'smoothness_worst',
+       'compactness_worst', 'concavity_worst', 'concave points_worst',
+       'symmetry_worst', 'fractal_dimension_worst'],inplace= True)
+
+clf_svm = svm.SVC(gamma=0.001, C=100.0)
+
+y_svm = dados['diagnosis']  #atributo classe
+X_svm = dados.drop(columns='diagnosis') #atributos descritores
+
+X_train, X_test, y_train, y_test = train_test_split(X_svm, y_svm, test_size = 0.50)
+
+arvore_clf = tree.DecisionTreeClassifier()
+arvore_clf.fit(X_train,y_train)
+
+#cross-validation
+scores_svm = cross_val_score(arvore_clf, X_svm, y_svm, cv=10, scoring='accuracy')
+print("Acuracia: %0.2f precisão com um desvio padrão de %0.2f" % (scores_svm.mean(), scores_svm.std()))
+
+with open('modelo_treinado.pkl', 'wb') as arquivo:
+    pickle.dump(arvore_clf, arquivo)
---
+++ front_end.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+import numpy as np
+import pickle
+
+from back_end import X_train, y_train
+
+# Função para obter os valores dos campos de entrada e fazer previsões
+def fazer_previsao():
+    # Obter os valores de cada campo de entrada e transformá-los em um array NumPy
+    entrada = np.array([
+        float(radius_mean_entry.get()),
+        float(texture_mean_entry.get()),
+        float(perimeter_mean_entry.get()),
+        float(area_mean_entry.get()),
+        float(smoothness_mean_entry.get())
+    ]).reshape(1, -1)
+
+    print(entrada)
+
+    with open('modelo_treinado.pkl', 'rb') as arquivo:
+        modelo = pickle.load(arquivo)
+
+        modelo.fit(X_train, y_train)
+
+    # Fazer a previsão com o modelo treinado
+    previsao = modelo.predict(entrada)
+
+    # Exibir a previsão na janela
+    resultado_label.config(text=f"Diagnóstico: {previsao[0]}")
+
+# Criação da janela principal
+root = tk.Tk()
+root.title("Predizer")
+
+# Criação dos rótulos e campos de entrada
+radius_mean_label = tk.Label(root, text="radius_mean")
+radius_mean_label.grid(row=0, column=0)
+radius_mean_entry = tk.Entry(root)
+radius_mean_entry.grid(row=0, column=1)
+
+texture_mean_label = tk.Label(root, text="texture_mean")
+texture_mean_label.grid(row=1, column=0)
+texture_mean_entry = tk.Entry(root)
+texture_mean_entry.grid(row=1, column=1)
+
+perimeter_mean_label = tk.Label(root, text="perimeter_mean")
+perimeter_mean_label.grid(row=2, column=0)
+perimeter_mean_entry = tk.Entry(root)
+perimeter_mean_entry.grid(row=2, column=1)
+
+area_mean_label = tk.Label(root, text="area_mean")
+area_mean_label.grid(row=3, column=0)
+area_mean_entry = tk.Entry(root)
+area_mean_entry.grid(row=3, column=1)
+
+smoothness_mean_label = tk.Label(root, text="smoothness_mean")
+smoothness_mean_label.grid(row=4, column=0)
+smoothness_mean_entry = tk.Entry(root)
+smoothness_mean_entry.grid(row=4, column=1)
+
+# Criação do botão de predição
+predict_button = tk.Button(root, text="Predizer 'diagnosis'", command=fazer_previsao)
+predict_button.grid(row=5, column=0, columnspan=2)
+
+# Criação do rótulo para exibir o resultado
+resultado_label = tk.Label(root, text="")
+resultado_label.grid(row=6, column=0, columnspan=2)
+
+# Iniciar o loop da interface
+root.mainloop()
